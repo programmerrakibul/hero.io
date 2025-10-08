7dc66204e694528ec34da08081806ae34e5a30a6
Programmer Rakibul <rakibul00206@gmail.com>
2025-10-08 13:33:40 +0600

    no installed app error handled

--- src/components/pages/Installation/Installation.jsx
+++ src/components/pages/Installation/Installation.jsx
@@ -2,10 +2,13 @@ import { useContext, useState } from "react";
 import Container from "../../Container/Container";
 import AppContext from "../../../Contexts/AppContext";
 import AppCard from "../../AppCard/AppCard";
+import notFoundImg from "../../../assets/App-Error.png";
+import { useNavigate } from "react-router";
 
 const Installation = () => {
   const [sortOrder, setSortOrder] = useState("");
   const { installedApps } = useContext(AppContext);
+  const navigate = useNavigate();
   const displayApps = sortOrder
     ? installedApps.sort((a, b) => {
         if (sortOrder === "high") {
@@ -36,6 +39,7 @@ const Installation = () => {
               ({installedApps.length}) App Found
             </h3>
             <select
+              disabled={installedApps.length === 0}
               defaultValue="Sort By Size"
               onChange={(e) => setSortOrder(e.target.value)}
               className="select"
@@ -46,7 +50,34 @@ const Installation = () => {
             </select>
           </div>
 
-          <div className="space-y-3.5">{installedAppElements}</div>
+          {installedApps.length > 0 ? (
+            <div className="space-y-3.5">{installedAppElements}</div>
+          ) : (
+            <div className="flex flex-col gap-10 justify-center items-center my-6 py-4">
+              <div>
+                <img src={notFoundImg} alt="Not Found" />
+              </div>
+
+              <div className="text-center space-y-4">
+                <h1 className="text-5xl font-semibold">
+                  No applications are installed.
+                </h1>
+                <p className="text-[#627382] max-w-3xl mx-auto">
+                  Currently, there are no applications installed on your device.
+                  To begin using applications, please click on the "see all
+                  apps" button, which will take you to a screen where you can
+                  browse and install a variety of apps to suit your needs and
+                  preferences.
+                </p>
+                <button
+                  onClick={() => navigate("/apps")}
+                  className="btn font-semibold text-white bg-gradient-to-br from-[#632EE3] to-[#9F62F2]"
+                >
+                  See All Apps
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </Container>
     </section>
