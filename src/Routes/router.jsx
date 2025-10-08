fc77e7c1464fbcd2212ce6f5362735012a1a5d76
Programmer Rakibul <rakibul00206@gmail.com>
2025-10-08 12:59:29 +0600

    Route Error handled

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -4,11 +4,13 @@ import HomePage from "../components/pages/HomePage/HomePage";
 import Apps from "../components/pages/AllApps/AllApps";
 import Installation from "../components/pages/Installation/Installation";
 import AppDetails from "../components/pages/AppDetails/AppDetails";
+import Error from "../components/pages/Error/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
--- src/components/pages/Error/Error.jsx
+++ src/components/pages/Error/Error.jsx
@@ -1,5 +1,39 @@
+import Container from "../../Container/Container";
+import Footer from "../../Footer/Footer";
+import Navbar from "../../Navbar/Navbar";
+import ErrorImg from "../../../assets/error-404.png";
+import { useNavigate } from "react-router";
+
 const Error = () => {
-  return <div>Page Not found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Navbar />
+      <section className="min-h-[calc(100dvh-100px)] flex justify-center items-center my-8 py-4">
+        <Container>
+          <div className="flex flex-col justify-center  items-center gap-10">
+            <div>
+              <img src={ErrorImg} alt="404" />
+            </div>
+            <div className="text-center space-y-4">
+              <h1 className="text-5xl font-semibold">Oops, page not found!</h1>
+              <p className="text-[#627382]">
+                The page you are looking for is not available.
+              </p>
+              <button
+                onClick={() => navigate("/")}
+                className="btn font-semibold text-white bg-gradient-to-br from-[#632EE3] to-[#9F62F2]"
+              >
+                Go Back
+              </button>
+            </div>
+          </div>
+        </Container>
+      </section>
+      <Footer />
+    </>
+  );
 };
 
 export default Error;
