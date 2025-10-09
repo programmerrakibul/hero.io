4099712ee58c4597c02c426160ef307b52081809
Programmer Rakibul <rakibul00206@gmail.com>
2025-10-09 20:17:12 +0600

    Charts Custom tooltip added

--- src/components/RatingChart/RatingChart.jsx
+++ src/components/RatingChart/RatingChart.jsx
@@ -1,3 +1,4 @@
+import { Star } from "lucide-react";
 import {
   Bar,
   BarChart,
@@ -10,6 +11,30 @@ import {
 } from "recharts";
 
 const RatingChart = ({ ratings }) => {
+  const CustomTooltip = ({ payload, active }) => {
+    if (active) {
+      const {
+        payload: { name, count },
+      } = payload[0];
+
+      return (
+        <div className="backdrop-blur-2xl p-3 rounded-lg w-48 sm:w-auto">
+          <p className="flex items-center justify-center gap-1.5">
+            <span>
+              {count} users rated this app {name}
+            </span>
+            <Star
+              className="inline-block animate-pulse"
+              size={16}
+              fill="#FF8811"
+              color="#FF8811"
+            />
+          </p>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="h-64 sm:h-80 my-10 capitalize text-sm sm:text-base">
       <ResponsiveContainer width="100%" height="100%">
@@ -20,7 +45,7 @@ const RatingChart = ({ ratings }) => {
           data={ratings}
         >
           <CartesianGrid stroke="transparent" />
-          <Tooltip />
+          <Tooltip content={<CustomTooltip />} />
           <XAxis type="number" />
           <YAxis dataKey="name" type="category" reversed />
           <Bar fill="#FF8811" dataKey="count" />
