d23ce2a017d4e7517222a9443836e96ea0722806
Programmer Rakibul <rakibul00206@gmail.com>
2025-10-09 22:27:39 +0600

    stats modified

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -11,46 +11,28 @@ const Stats = () => {
           </h2>
 
           <div className="flex flex-wrap items-center justify-center gap-16">
-            <div className="flex flex-col md:flex-row items-center gap-2.5 md:gap-4">
-              <div className="w-16 lg:w-20 animate-bounce">
-                <CloudDownload size="100%" color="#44FFD195" />
-              </div>
-
-              <div className="space-y-2.5">
-                <p className="opacity-80 text-base">Total Downloads</p>
-                <p className="font-extrabold text-4xl md:text-5xl lg:text-6xl">
-                  29.6M
-                </p>
-                <p className="opacity-80 text-base">21% more than last month</p>
-              </div>
+            <div className="space-y-2.5">
+              <p className="opacity-80 text-base">Total Downloads</p>
+              <p className="font-extrabold text-4xl md:text-5xl lg:text-6xl">
+                29.6M
+              </p>
+              <p className="opacity-80 text-base">21% more than last month</p>
             </div>
 
-            <div className="flex flex-col md:flex-row items-center gap-2.5 md:gap-4">
-              <div className="w-16 lg:w-20  animate-bounce ">
-                <Star size="100%" color="#FF8811" />
-              </div>
-
-              <div className="space-y-2.5">
-                <p className="opacity-80 text-base">Total Reviews</p>
-                <p className="font-extrabold text-4xl md:text-5xl lg:text-6xl">
-                  906K
-                </p>
-                <p className="opacity-80 text-base">46% more than last month</p>
-              </div>
+            <div className="space-y-2.5">
+              <p className="opacity-80 text-base">Total Reviews</p>
+              <p className="font-extrabold text-4xl md:text-5xl lg:text-6xl">
+                906K
+              </p>
+              <p className="opacity-80 text-base">46% more than last month</p>
             </div>
 
-            <div className="flex flex-col md:flex-row items-center gap-2.5 md:gap-4">
-              <div className="w-16 lg:w-20  animate-bounce">
-                <ShieldCheck size="100%" color="#388E3C" />
-              </div>
-
-              <div className="space-y-2.5">
-                <p className="opacity-80 text-base">Active Apps</p>
-                <p className="font-extrabold text-4xl md:text-5xl lg:text-6xl">
-                  132+
-                </p>
-                <p className="opacity-80 text-base">31 more will Launch</p>
-              </div>
+            <div className="space-y-2.5">
+              <p className="opacity-80 text-base">Active Apps</p>
+              <p className="font-extrabold text-4xl md:text-5xl lg:text-6xl">
+                132+
+              </p>
+              <p className="opacity-80 text-base">31 more will Launch</p>
             </div>
           </div>
         </div>
