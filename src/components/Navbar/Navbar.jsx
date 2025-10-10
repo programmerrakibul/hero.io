96deb06a1e4ec1b9aa49498605df30a8dbbd6075
Programmer Rakibul <rakibul00206@gmail.com>
2025-10-10 20:10:03 +0600

    Navbar modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,16 +19,16 @@ const Navbar = () => {
   ));
 
   return (
-    <nav className="bg-white border-b border-[#E9E9E9]">
+    <nav className="bg-white border-b border-[#E9E9E9] sticky top-0 z-10">
       <Container>
         <div className="navbar p-0">
           <div className="navbar-start gap-1">
             <Link
               to=".."
-              className="flex items-center justify-center gap-0.5 sm:gap-1.5"
+              className="flex items-center justify-center gap-1.5"
             >
-              <img src={logo} alt="Logo" className="size-7 sm:size-10" />
-              <span className="bg-gradient-to-tr from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent text-base sm:text-lg font-bold">
+              <img src={logo} alt="Logo" className="size-10" />
+              <span className="bg-gradient-to-tr from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent text-lg font-bold">
                 HERO.IO
               </span>
             </Link>
